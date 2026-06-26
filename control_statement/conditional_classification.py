d415a60acea0a90606cf3d3ed6b996c02425df82
Rimjhimm12 <rimjhimmallick6@gmail.com>
2026-06-25 19:42:14 -0500

    updated

--- control_statement/conditional_classification.py
+++ control_statement/conditional_classification.py
@@ -1,8 +1,12 @@
 #Check whether the given number is positive or negative.
-number = float(input("Enter a number: "))
-if number>0:
-    print("This is a positive number")
-elif number==0:
-    print("This is zero")
+try:
+    number = float(input("Enter a number: "))
+except ValueError:
+    print("Please enter a number ")
 else:
-    print("This is a negative number")
+    if number > 0:
+        print("This is a positive number")
+    elif number == 0:
+        print("This is zero")
+    else:
+        print("This is a negative number")
