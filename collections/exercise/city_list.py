1e1e87ab8ee747fefcb339db296bdf560986083d
Rimjhimm12 <rimjhimmallick6@gmail.com>
2026-07-07 23:10:23 -0500

    updated

--- collections/exercise/city_list.py
+++ collections/exercise/city_list.py
@@ -1,5 +1,6 @@
+
+'''You have a list of city names. Check whether "Chicago" exists.'''
 city_list = ["Dallas","Denver", "Chicago", "New York", "Los Angeles"]
 for city in city_list:
     if city.__contains__("Chicago"):
         print('Chicago is in the list')
-    
---
+++ collections/exercise/username.py
@@ -0,0 +1,5 @@
+
+''' Store usernames. Duplicate usernames should never be stored.'''
+user_name = {"RM12", "CR7", "LM10","KM27","RM12", "CR7"}
+print(user_name)
+
