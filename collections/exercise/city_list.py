f1880ba77735f12a03107629b2f8a1fde9ca25e2
Rimjhimm12 <rimjhimmallick6@gmail.com>
2026-07-08 16:26:14 -0500

    Updated syntax and logic

--- collections/exercise/city_list.py
+++ collections/exercise/city_list.py
@@ -1,6 +1,14 @@
 
 '''You have a list of city names. Check whether "Chicago" exists.'''
 city_list = ["Dallas","Denver", "Chicago", "New York", "Los Angeles"]
+new_city = "Chicago"
 for city in city_list:
-    if city.__contains__("Chicago"):
-        print('Chicago is in the list')
+    if city == new_city:
+        print(city,'is present in the list')
+
+
+
+
+
+
+
