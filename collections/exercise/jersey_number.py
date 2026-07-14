17c1d3ddfb0fe9d6b9443ba9254eed9283c7ec1d
Rimjhimm12 <rimjhimmallick6@gmail.com>
2026-07-14 10:34:12 -0500

    Created new Python file

---
+++ collections/exercise/jersey_number.py
@@ -0,0 +1,8 @@
+players_jersey_no={2,3,4,2,5,6,3}
+players_jersey_no_list=[2,9,8,3]
+print(players_jersey_no)
+#or
+for i in players_jersey_no_list:
+    if i not in players_jersey_no:
+        players_jersey_no.add(i)
+print(players_jersey_no)
