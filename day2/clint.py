b35e44b9b66d78691016e1979085cc647487c096
Rimjhimm12 <rimjhimmallick6@gmail.com>
2026-07-21 16:55:02 -0500

    updated

---
+++ day2/clint.py
@@ -0,0 +1,7 @@
+import sys
+sys.path.append("/Users/rimjhim/PycharmProjects/LearningPythonProject/pack1")
+sys.path.append("/Users/rimjhim/PycharmProjects/LearningPythonProject/pack1/pack2")
+from module1 import *
+import module3
+display()
+module3.show()
---
+++ pack1/pack2/module3.py
@@ -0,0 +1,2 @@
+def show():
+    print("This is a function inside module3")
